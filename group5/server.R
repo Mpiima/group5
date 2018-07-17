5028fcf9c132b08e575f30950bc19cb4e383276a
Guma Evan <gumanyarevan@gmail.com>
2018-07-17 12:15:55 +0100

    side app developed for later(merging)

--- group5/server.R
+++ group5/server.R
@@ -1,48 +1,163 @@
 library(shiny)
+library(shinydashboard)
+library(data.table)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(lubridate)
+library(tm)
+library(plotrix)
 options(shiny.maxRequestSize=50*1024^2)
-server <- function(input,output,session){
-  #implementing file input##############
-  myData <- reactive({
-    file1<-input$file
-    if(is.null(file))return(NULL)
+shinyServer(
+ 
+  function(input,output,session){ #data processing implementation
+    myData <- reactive({
+      file1<-input$file
+      if(is.null(file))return(NULL)
+      
+      data<-read.csv(file1$datapath, header = TRUE)
+      
+    })
     
-    data<-read.csv(file1$datapath, header = TRUE)
-    data
-  })
-  ###########end and start of output uploaded data#######
-  output$contents <- renderTable(
-    myData()
-  )
-  ####eerror ########
-  read<-function(){
-    if(is.null(input$file))
-      return(NULL)
-    library(ggplot2)
-    
-    infile<-input$file
-    data<-read.csv(infile$datapath)
-    
-    #######variables for the data titles in the  set#########
-    df<-data.frame(
-      Date=data$Date,
-      Symbol=data$Symbol,
-      Open=data$Open,
-      Close=data$Close,
-      Volume=data$Volume,
-      MarketCap=data$MarketCap
-    
+    output$contents <- renderTable(
+      myData()
     )
-    df ####reading the output#############################
-  }
-  # reading the two tables in the data frame###############
-  output$mytable1<-renderDataTable({read()[,1:6]})
-  
-##############end of output and start of analysis in close price barplot####################################################
-  
-  output$closeprice <- renderPlot({
-    d <- 
+    read<-function(){
+      if(is.null(input$file))
+        return(NULL)
+      library(ggplot2)
+      
+      infile<-input$file
+      data<-read.csv(infile$datapath)
+      
+      df<-data.frame(
+        Currency=data$Currency,
+        Date=data$Date,
+        Open=data$Open,
+        High=data$High,
+        Low=data$Low,
+        close =data$close,
+        Volume=data$Volume,
+        MarketCap=data$MarketCap
+       
+      )
+      df 
+    }
+    output$mytable1<-renderDataTable({read()[1:8]}) # reading the one table in the data frame and end data imple..
     
-    barplot(data$Close,data$Date, ylab = "close price", xlab = "date")
-  })
-###############end in closeprice analysis and start in correlation analsis#############
+    #.....................................IMPLEMENTATION IN CLOSE PRICE...........................................#
+    library(reshape2)
+    #Change data frame with row=date and col=close.price
+    close.raw <- reshape(data[c(1,2,6)], timevar= "Currency", idvar = "Date", direction = "wide")
+    close.raw[,"Close.Currency"] <- NULL
+    #Change values into numeric and get it as the dataframe style
+    close <- data.frame(sapply(close.raw, function(z){as.numeric(as.character(z))}))
+    #Change names of cols
+    colnames(close) <- sub("close.", "", colnames(close))
+    #Change the date column into POSIXct style
+    library(lubridate)
+    dates <- parse_date_time(x = as.character(close.raw$Date), orders ="b/d/Y", locale = "eng")
+    close$Date<- dates
+    #Delete the last row since it has no information
+    close <- close[-nrow(close),]
+    x<-dim(close)
+    
+    length.col <- colSums(!is.na(close[,-1,2]))
+    
+    #Newest cryptocurrency in this dataset
+    sort(length.col)[1]
+    #Summary table about how long the cryptocurrencies are. Unit is day.
+    options("scipen"=100, "digits"=4)
+    table1<- table(cut(length.col, c(0, 180, 365, 2*365, 3*365, 4*365, Inf), right = T))
+    #Percentage
+    table2<-table(cut(length.col, c(0, 180, 365, 2*365, 3*365, 4*365, Inf), right = T))/200*100
+    #CORRELATION analysis
+    
+    #Take cols with more than 200
+    #correlation between all cryptocurrencies
+    close.1440 <- close[,colSums(!is.na(close)) >= 1440]
+    close.1440
+    corr <- cor(close.1440[,-1], use = "pairwise.complete")
+   library(corrplot)
+    corr.bit <- corr[1:ncol(corr)]
+   corr.bit
+   #corr.bit.dec.order <- corr.bit[order(corr.bit, decreasing=T),,drop=F]
+   #corr.bit.inc.order <- corr.bit[order(corr.bit, decreasing=F),,drop=F]
+   
+    #correlation btn btc and alt
+    #corrplot(t(corr.bit), diag = FALSE, tl.col = "black", tl.cex = 0.7, mar = c(0,1,2,0))
+    
+    #,,,,,,,,,,,,,,,,,,,,,,,,,MARKET CAP,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+   
+    #.....................................END IMPLE...............................................................#
+    
+    markcap <- data[c(1,2,8)]
+    markcap$Market.Cap[markcap$Market.Cap == "-"] 
+    #Change data frame with row=date and col=close.price
+    markcap.raw <- reshape(markcap, timevar= "Currency", idvar = "Date", direction = "wide")
+    markcap.raw[,"Market.Cap.Currency"] <- NULL
+    #Change values into numeric
+    markcap <- sapply(markcap.raw, function(z){as.numeric(gsub(",","", z))})
+    #Make data data.frame while deleting the last row because this has no info
+    markcap <- data.frame(markcap[-nrow(markcap),])
+    #Take cols with more than 200
+    markcap <- markcap[,colSums(!is.na(markcap)) >= 200]
+    #Change names of cols
+    colnames(markcap) <- sub("Market.Cap.", "", colnames(markcap))
+    #average market cap
+    mean.cap <- data.frame(mean.cap=colMeans(markcap, na.rm = T))
+    str(mean.cap)
+    mean.cap.10.name <- rownames(mean.cap[order(mean.cap$mean.cap, decreasing = T),,drop=F])[1:10]
+    mean.cap.10.value <- mean.cap[order(mean.cap$mean.cap, decreasing = T),,drop=F][1:10,]
+    mean.cap.10 <- data.frame(name=mean.cap.10.name, mean.market.cap=mean.cap.10.value)
+   str(mean.cap.10)
+   # barplot(mean.cap.10[,2], names.arg = mean.cap.10[,1],las=2 , cex.names=0.9,
+         #   main="Average market capital in top 10 Cryptocurrencies")
+   
+    #*****************************OUTPUTS*********************************************************************
+    
+    output$statement<-renderText({
+      paste("there are ", x[1], "rows", " and ", x[2], "cryptocurrencies" )
+    })
+    output$crypto <- renderText({
+      paste("newest crypto is ", sort(length.col)[1])
+    })
+    output$dayunit<-renderTable({
+      table(cut(length.col, c(0, 180, 365, 2*365, 3*365, 4*365, Inf), right = T))
+    })
+    output$per <- renderTable({
+      table(cut(length.col, c(0, 180, 365, 2*365, 3*365, 4*365, Inf), right = T))/200*100
+    })
+    
+    output$pie1 <- renderPlot({
+      pie(table2)
+    })
+    #correlation btn all cryptocurrencies
+    output$data2<-renderDataTable({
+      table(corr.bit)
+    })
+    output$his1 <- renderPlot({
+      corrplot(corr, order="hclust", diag = FALSE, tl.col = "black", tl.cex = 0.7,
+               title = "Correlation matrix (ordered by hierarchical clustering)",
+               mar = c(0,1,2,0))
+    })#bitcoins and alt 
+    output$his2 <- renderPlot({
+      plot(t(corr.bit), diag = FALSE, tl.col = "black", tl.cex = 0.8, mar = c(0,1,2,0))
+    })
+    output$table5<-renderTable({
+      table(markcap)
+    })
+    output$table4 <-renderTable({#data for mean of market cap
+      mean.cap.1<-data.table(mean.cap.10)
+    })
+    
+    output$bar1<-renderPlot({
+     barplot(mean.cap.10[,2], names.arg = mean.cap.10[,1],las=2 , cex.names=0.9,
+             main="Average market capital in top 10 Cryptocurrencies")
+    })
+    output$data5<-renderDataTable({
+      data
+    })
+   
   }
+)
--- group5/ui.R
+++ group5/ui.R
@@ -11,114 +11,125 @@ library(memisc)
 library(sqldf)
 library(lubridate)
 library(tm)
-library(wordcloud)
-library(RColorBrewer)
-library(quanteda)
-library(syuzhet)
-library(maps)
-library(mapdata)
-library(forecast)
-library(plotrix)
 
-#desiging a user interface
-shinyUI(
-  
-  dashboardPage(
-    dashboardHeader(title = em("CRYPTOCURRENCY ANALYSIS PROJECT"),titleWidth = "500"),
+#This section begins the user interfacez
+ui <- fluidPage(theme="style.css",
+                tags$img(src="crypto7.png", width=100,height=100),
+  dashboardHeader(title = "CRYPTOCURRENCY DATA ANALYSIS PROJECT",titleWidth = "500"),
+
+  dashboardSidebar(
+    #tags$style("body{background:black;color:brown}"),
+  #tags$img(src="one.jpg", width=100,height=100),
+    #menuItem("PROJECT MEMBERS",tabName = "MEMBERS", icon =icon("data")),
     
-    dashboardSidebar(
-      
-      #cryptocurrency image
-      tags$img(src='image2.jpg', width="100"),
-      ##########search input##################
-      sidebarSearchForm(label = "Enter a number", "searchText", "searchButton"),
-      
-      #######end of search#####################################
-      sidebarMenu(
-        
-        
-        
-        ###############uploadfile########
-      menuItem("RawData Upload",tabName = "upload", icon = icon("upload"),
-               menuSubItem(icon = icon("upload"),
-                           fileInput(inputId ="file","choose File",
-                                     accept =c('text/csv','text/comma-separated-values,text/plain','.csv')) )),
-      ##########end of uploading file AND start of data#################
-      
-      
-      
-      menuItem("Data", tabName = "data", icon = icon("table")),
-      ####33end of data and begin closeprice analysis with all graphs######
-      
-      menuItem(strong("ANALYSIS IN CLOSE PRICE"), tabName = "closeprice"),
-      menuSubItem("barplot",tabName = "closepricecharts",icon = icon("bar-chart-o")),
-      menuSubItem("Linegraph", tabName = "closepricelinegraph", icon = icon("line-chart")),
-      menuSubItem("scatter",tabName = "closepricescatter",icon = icon("line-chart")),
-      #######end of analysis in closeprice and start correlation analysis################
-      
-      menuItem(strong("CORRELATION ANALYSIS"), tabName=""),
-      menuSubItem("btn all cryptocurrencies", tabName = "all", icon = "bar-chart-o"),
-      menuSubItem(" Top5+correlated-with-bitcoin", tabName = "+bitcoins", icon = icon("table")),
-      menuSubItem(" Top5-correlated-with-bitcoin", tabName = "-bitcoins", icon = icon("table")),
-      
-      #####end of correlation analysis and begin in analysis in market cap
-      menuItem(strong("MARKET CAP ANALYSIS"), tabName = "market capital"),
-      menuSubItem("average market cap", tabName = "mrktcap", icon = icon("bar-chart-o")),
-      
-      
-      ######end in mkt cap and begin top 10 analysis###########
-      
-      
-      menuItem(strong("TOP 10 ANALYSIS"),tabName = "top 10"),
-      ######end of top 10 and begin predict ######
-      menuSubItem("mean", tabName = "mean", icon = icon("table")),
-      menuSubItem(" variance", tabName = "variance", icon = icon("table")),
-      menuSubItem("log-return", tabName = "log", icon = icon("table")),
-      
-      
-      menuItem(strong("predict")),
-      #######end predict and then project description
-      menuItem("Project Description")
-    )),
     
-    ###########begin of the body#############
-    dashboardBody(
-      tabItems(
-        #defining data output system
-        tabItem(tabName = "data",
-          ###start############
-          tabsetPanel(
-            type = "tab",
-            tabsetPanel(id="tabs",
-                        tabPanel(value="panel1", "uploaded_data",icon = icon("table"), dataTableOutput("mytable1"),dataTableOutput("mytable2")))
-          )
-        )#end tab and start of analysis in close price
-                    
-        ),
-      textOutput(bash),
-      tabItem(tabName = "closeprice",
-              tabsetPanel(
-                type="tab",
+  sidebarMenu( 
+    
+    
+    menuItem("RawData Upload",tabName ="upload",icon =icon("upload"),
+                                         menuSubItem(icon = icon("none"),
+                                                     
+                    fileInput(inputId ="file","choose File",accept =c('text/csv','text/comma-separated-values,text/plain','.csv')))),
+   
+                                menuItem("Data",tabName ="data",icon =icon("table")),
+                                menuItem("ANALYSIS IN CLOSE PRICE",tabName = "closeprice", icon =icon("pie-chart")),
+                                menuItem("CORRELATION ANALYSIS",tabName = "cor",icon = icon("bar-chart")),
+                                menuItem("ANALYIS IN MARKET CAP",tabName = "cap",icon = icon("bar-chart")),
+                                menuItem("TOP 10 ANALYSIS",tabName = "cap",icon = icon("bar-chart")),
+                                menuItem("DESTRIBUTION ANALYSIS",tabName = "cap",icon = icon("bar-chart")),
+                                menuItem("PREDICT",tabName = "predict")
+                               #menuItem("",tabName = "cap",icon = icon("bar-chart"))
+    
+                                
+
+  )
+  ),
+  #here is start the body
+  dashboardBody(
+    
+    
+    tabItems(
+      tabItem(tabName = "data",
                 tabsetPanel(id="tabs",
-                            tabPanel(value="panel1", "analysis in close price",icon = icon("table"),
-                                     box(width=12,tags$b("close price analysis"),
-                                         p("The table shows the analysed close prices of all of cryptocurrencies")),
-                                     box(plotOutput("cryptocurrencies"))
-                            ),
-                            ######tab for the barcharts
-                            tabPanel(tabName="closepricecharts",
-                                     box(plotOutput("closeprice"),width = 100,height = 100)
-                            )
-                            ####end
-                            
-                ))
+                            tabPanel(value="panel1", "Raw-Data",icon = icon("table"), dataTableOutput("data5"))
+                ))),#tabitems end for data processing
+    #,,,,,,,,,,,,,,,,,,,,,,,closeprice analysis,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,#
+    tabItems(
+      tabItem(tabName = "closeprice",
+              tabsetPanel(id="tabs",
+                          tabPanel(value="panel", "Statement",icon = icon("table"),
+                                   tags$b("The newest cryptocurrency"),
+                                       p("The table shows how long the cryptocurrency has been on market and sorts out the newest cryptocurrency"),
+                                   
+                                   textOutput("statement"),
+                                   textOutput("crypto")),
+                                 tabPanel("SummaryData",p("Summary table about how long the cryptocurrencies when unit is a day"), tableOutput("dayunit"),
+                                  p("Summary table about how long the cryptocurrencies in percentage"),tableOutput("per")),
+                                 tabPanel("Plot", plotOutput("pie1")))
+                                   
+                          
+              )),#tabitems for close price end
+    #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,correlation analysis,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+    tabItems(
+      tabItem(tabName = "cor",
+              tabsetPanel(id="tabs",
+                          #data for ploting cor plots
+                          tabPanel("summarised data", tableOutput("data2"),downloadButton("downloadc1","download")),
+                          #corr for all
+                          tabPanel(value="panel3", "Correlation between all",icon = icon("bar-chart"),sliderInput("cor.bit[1]", "Input", 1444, 2888, c(1444, 2888)),
+                                   plotOutput("his1"),downloadButton("downloadc2","downloadPlot")),
+                                  tabPanel("bitcoins and alt",plotOutput("his2"),downloadButton("downloadc3","downloadPlot")),
+                          #top 5 correlation with BTC
+                          tabPanel("Top5 correlation with BTC",tableOutput("top5positive"),br(),tableOutput("top5negative")))
+              )),
+    
+    #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,market cap analysis,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+    tabItems(
+      tabItem(tabName = "cap",
+              tabsetPanel(id="tabs",
+                          tabPanel(value="panel3", "Average market cap",icon = icon("table"),
+                                   tableOutput("table4")),
+                                  tabPanel("Top 10 average market cap",tableOutput("table6")),
+                                 tabPanel("Plot for Top 10",plotOutput("bar1"))
+                                
+              ))),
+    
+    strong("WELCOME TO WORLD OF CRYPTOCURENCY"),
+    hr(),h2("About our Data set"),
+    p("Recent growing interest in cryptocurrencies, specifically as a speculative investment vehicle, has sparked global conversation over the past 12 months. 
+      Although this data is available across various sites, there is a lack of understanding as to what is driving the exponential rise of many individual currencies. 
+      This data set is intended to be a starting point for a detailed analysis into what is driving price action, and what can be done to predict future movement."),
+   hr(),
+   h2("contents"),
+   p("Consolidated financial information for the top 736 cryptocurrencies by marketcap. Attributes include:
+
+     Currency name (e.g. bitcoin)
+     Date
+     Open
+     High
+     Low
+     Close
+     Volume
+     Marketcap
+     "),
+   p("Questions to be answered:
+
+     What is the correlation between bitcoin and alt coin prices?
+     What is the average age of the top 10 coins by market cap?
+     What day of the week is best to buy/sell?
+     Which coins in the top two hundred are less than 6 months old?
+     Which currencies are the most volatile? "),
+     tabItems(#names of members
+      tabItem(tabName = "MEMBERS",
               
-              
-              )
-      ############tab for the charts here in the close prices#############
-      
-      ############end of the chartsin closeprice and##################
-      )
-      ############end of the body#################
-    ))
-  
+                          tabPanel(value="panel3",h3("MEMBERS AND THERE PROFILES"),
+                                   h3("Name: MPIIMA COLINS"),tags$img(src="one.jpg", width=100,height=100), h6("contact: 0786950567"),h6("Email:collinsmpiima@gmail.com"),h6("Regno.16/U/7120/ps"),br(),
+                                   
+                                   h3("Name: GUMA IVAN"),tags$img(src="one.jpg", width=100,height=100), h6("contact: 0786950567"),h6("Email:gumaivan@gmail.com"),h6("Regno.16/U/7120/ps"),br(),
+                                   h3("Name: NATWIJUKIRE CRISPUS"),tags$img(src="one.jpg", width=100,height=100), h6("contact: 0786950567"),h6("Email:natwijukirecrispus@gmail.com"),h6("Regno.16/U/7120/ps"),br(),
+                                   h3("Name: NYANZI SAMUEL"),tags$img(src="one.jpg", width=100,height=100), h6("contact: 0786950567"),h6("nyanzisamuel.com"),h6("Regno.16/U/7120/ps"),br()
+                          
+                         
+              )))
+  )#body end
+)
